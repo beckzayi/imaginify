c9adbb4a8d038d22ff55bd413cd1fc8087cc5069
Darren Li <darrenli@hipagesgroup.com.au>
2024-05-02 18:01:53 +1000

    clerk webhook console.log

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -57,6 +57,8 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
+  console.log('evt:', evt);
+
   // CREATE
   if (eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } =
